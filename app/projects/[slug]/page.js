9276125f34ef1846b854a9421e04b948f54cf682
tejasmachhi <tejasmachhi2710@gmail.com>
2026-03-01 09:29:58 +0530

    fix bug

--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -1,4 +1,10 @@
+import { getAllProjectSlugs } from '@/data/projects';
 import ProjectDetail from '@/features/projectDetail/ProjectDetail';
+
+export async function generateStaticParams() {
+  const slugs = getAllProjectSlugs();
+  return slugs.map(slug => ({ slug }));
+}
 
 export default function ProjectDetailPage({ params }) {
   return <ProjectDetail slug={params.slug} />;
